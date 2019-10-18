bb968708ad58d9afc91bc044fadb9b758c112f95
Corina Allen <StarPerfect@gmail.com>
2019-10-18 09:29:22 -0600

    Initial commit - iteration 0 complete

---
+++ test/node_test.rb
@@ -0,0 +1,19 @@
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/node'
+
+class NodeTest < Minitest::Test
+  def setup
+    @node = Node.new('Corina')
+  end
+
+  def test_exists
+    assert_instance_of Node, @node
+  end
+
+  def test_attributes
+    assert_equal 'Corina', @node.surname
+    assert_nil @node.next_node
+  end
+end
